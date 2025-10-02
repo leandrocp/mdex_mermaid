94be9c5dffc5c4b011259aefa17e073414e9df22
Leandro Pereira <leandro@leandro.io>
2025-10-02 11:59:57 -0400

    v0.3.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MDExMermaid.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leandrocp/mdex_mermaid"
-  @version "0.3.2-dev"
+  @version "0.3.2"
 
   def project do
     [
