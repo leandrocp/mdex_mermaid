a3ff6103c4310a39748c8cb9a5afea175a78a424
Leandro Pereira <leandro@leandro.io>
2025-10-09 19:06:18 -0400

    -dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## Unreleased
+
 ## 0.3.2 - 2025-10-02
 
 ### Changed
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MDExMermaid.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leandrocp/mdex_mermaid"
-  @version "0.3.2"
+  @version "0.3.3-dev"
 
   def project do
     [
